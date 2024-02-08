d18361022bb01e40711baea77bb942ad177900bb
Yash <02yashrajput@gmail.com>
2024-02-08 18:36:20 +0530

    before header

--- movies_database/src/App.js
+++ movies_database/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import{Routes,Route}from "react-router-dom"
+import Home from './pages/Home';
 function App() {
   return (
-   <div>
-    Hello
-   </div>
+   <Routes>
+     <Route path="/" element={<Home />} />
+   </Routes>
   );
 }
 
---
+++ movies_database/src/context/AppContent.js
@@ -0,0 +1,8 @@
+import { createContext} from "react";
+
+export const AppContext = createContext();
+
+export default function AppContextProvider({ children }) {
+    const value = {}
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>
+}
