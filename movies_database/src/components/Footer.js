5473aeadb7e7ddc4136005b367a1423e39850820
Yash <02yashrajput@gmail.com>
2024-02-09 19:38:27 +0530

    after footer complete

--- movies_database/src/components/Footer.js
+++ movies_database/src/components/Footer.js
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import {
+  FaInstagramSquare,
+  FaGithub,
+  FaLinkedin,
+  FaFacebookSquare,
+  FaEnvelope
+} from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className='fixed bottom-0 left-0 right-0'>Footer</div>
-  )
-}
+    <div className="absolute bottom-0 left-0 right-0 flex flex-col justify-center items-center h-64   text-slate-500 gap-y-5 border-t border-slate-500">
+      <div className="text-4xl  font-bold">Movies-DataBase</div>
+      <div className="flex gap-7 text-3xl ">
+        <a href="/"  className=" hover:bg-slate-900 hover:bg-opacity-15 dark:hover:bg-white dark:hover:bg-opacity-5 p-3 rounded-full">
+        <FaEnvelope />
 
-export default Footer
+        </a>
+        <a href="https://www.instagram.com/iam_y.r/"  className=" hover:bg-slate-900 hover:bg-opacity-15 dark:hover:bg-white dark:hover:bg-opacity-5 p-3 rounded-full">
+          <FaInstagramSquare />
+        </a>
+        <a href="https://github.com/02YashRajput"  className=" hover:bg-slate-900 hover:bg-opacity-15 dark:hover:bg-white dark:hover:bg-opacity-5 p-3 rounded-full">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/yash-rajput-54b763282/" className=" hover:bg-slate-900 hover:bg-opacity-15 dark:hover:bg-white dark:hover:bg-opacity-5 p-3 rounded-full">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=100083846002768" className=" hover:bg-slate-900 hover:bg-opacity-15 dark:hover:bg-white dark:hover:bg-opacity-5 p-3 rounded-full">
+          <FaFacebookSquare />
+        </a>
+      </div>
+      <div>
+        Made By- <span className="text-xl font-bold">Yash Rajput</span> 
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
