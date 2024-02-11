8c26ea093d2e1e0ae756ba21c5516ac5aa37ad46
Yash <02yashrajput@gmail.com>
2024-02-11 12:19:06 +0530

    before big decision

--- movies_database/src/App.js
+++ movies_database/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import{Routes,Route}from "react-router-dom"
 import Home from './pages/Home';
-
+import ParticularPage from './pages/ParticularPage';
 function App() {
 
   
@@ -10,6 +10,7 @@ function App() {
   return (
    <Routes>
      <Route path="/" element={<Home />} />
+     <Route path='/:id' element={<ParticularPage/>}/>
    </Routes>
   );
 }
--- movies_database/src/components/Container.jsx
+++ movies_database/src/components/Container.jsx
@@ -1,34 +1,96 @@
-import { FaChevronRight } from "react-icons/fa";
+import { useEffect, useRef, useState, useLayoutEffect } from "react";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
-const Container = ({itemName,data }) => {
-  return (
-    <div className='w-full h-[27rem] text-slate-500 px-5 flex flex-col justify-evenly'>
-        <div className='flex items-center justify-between '>
-        <h1 className='text-2xl'>{itemName.toUpperCase().split("_").join(" ")}</h1>
-        <FaChevronRight />
-        </div>
-        <div className=' overflow-hidden h-[22rem]  relative flex gap-5 '>
+const Container = ({ itemName, data }) => {
+    const scrollRef = useRef(null);
+    const leftButton = useRef(null);
+    const rightButton = useRef(null);
 
-        {
-            data.map((value,index)=>{
-                return(
-                    <div key={index} className='h-[22rem] min-w-60 flex justify-around flex-col items-center'>
-                        
-                        <img src={`https://image.tmdb.org/t/p/w500/${value.poster_path}` } className="h-72 w-full" alt={`${value.poster_path}`}/>
-                        <h3 className="text-xl font-bold">{
-                                value.original_title || value.original_name
-                                    ? (value.original_title || value.original_name).length > 18
-                                        ? `${(value.original_title || value.original_name).substring(0, 18)}...`
-                                        : value.original_title || value.original_name
-                                    : "Title not available"
-                            }</h3>
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const [scrollWidth, setScrollWidth] = useState(0); 
+    // Use useLayoutEffect to get the scroll width immediately
+    useLayoutEffect(() => {
+        const scrollElement = scrollRef.current;
+        if (scrollElement) {
+            const width = scrollElement.scrollWidth;
+            setScrollWidth(width);
+        }
+    }, [data]);
+
+    useEffect(() => {
+        const scrollElement = scrollRef.current;
+
+        const handleScroll = () => {
+            if (scrollElement) {
+                setScrollPosition(scrollElement.scrollLeft);
+            }
+        };
+
+        scrollElement.addEventListener("scroll", handleScroll);
+
+        return () => {
+            scrollElement.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        const scrollElement = scrollRef.current;
+
+
+        if (scrollPosition <= 0) {
+            leftButton.current.style.display = "none";
+        } else {
+            leftButton.current.style.display = "flex";
+        }
+
+        if (scrollPosition >= scrollWidth - scrollElement.clientWidth) {
+            rightButton.current.style.display = "none";
+        } else {
+            rightButton.current.style.display = "flex";
+        }
+    }, [scrollPosition, scrollWidth]); 
+    const handleScrollLeft = () => {
+        const scrollElement = scrollRef.current;
+        if (scrollElement) {
+            const scrollAmount = scrollElement.clientWidth * -1 / 2;
+            scrollElement.scrollBy({ left: scrollAmount, behavior: "smooth" });
+            setScrollPosition(scrollElement.scrollLeft + scrollAmount);
+        }
+    };
+
+    const handleScrollRight = () => {
+        const scrollElement = scrollRef.current;
+        if (scrollElement) {
+            const scrollAmount = scrollElement.clientWidth / 2;
+            scrollElement.scrollBy({ left: scrollAmount, behavior: "smooth" });
+            setScrollPosition(scrollElement.scrollLeft + scrollAmount);
+        }
+    };
+
+    return (
+        <div className='w-full h-[27rem] text-slate-500 px-5 flex flex-col justify-evenly relative'>
+            <div className='flex items-center justify-between'>
+                <h1 className='text-2xl'>{itemName.toUpperCase().split("_").join(" ")}</h1>
+                <FaChevronRight />
+            </div>
+            <button ref={leftButton} onClick={handleScrollLeft} className="absolute top-1/2 -translate-y-1/2 translate-x-2 left-0   z-10 h-10 w-10 rounded-full flex items-center justify-center bg-[rgba(0,0,0,0.7)]  dark:bg-black dark:text-white"><FaChevronLeft /></button>
+            <div ref={scrollRef} className='transition-all duration-800 ease-in-out overflow-x-scroll h-[22rem] overflow-y-hidden relative flex gap-5 hidingscrollbar'>
+                {data.map((value, index) => (
+                    <div key={index}  className='h-[22rem] min-w-60 flex justify-around flex-col items-center'>
+                        <img src={`https://image.tmdb.org/t/p/w500/${value.poster_path}`} className="h-72 w-full" alt={`${value.poster_path}`} />
+                        <h3 className="text-xl font-bold">
+                            {value.original_title || value.original_name
+                                ? (value.original_title || value.original_name).length > 18
+                                    ? `${(value.original_title || value.original_name).substring(0, 18)}...`
+                                    : value.original_title || value.original_name
+                                : "Title not available"}
+                        </h3>
                     </div>
-                    )
-                })
-}
-                </div>
-    </div>
-  )
+                ))}
+            </div>
+            <button ref={rightButton} onClick={handleScrollRight} className="absolute top-1/2 -translate-y-1/2 -translate-x-2 right-0  z-10 h-10 w-10 rounded-full flex items-center justify-center bg-[rgba(0,0,0,0.7)]  dark:bg-black dark:text-white"><FaChevronRight /></button>
+        </div>
+    );
 }
 
-export default Container
+export default Container;
