9e4eb90071400a4de89fe3d5e5f69d0fb5c39628
Yash Rajput <02yashrajput@gmail.com>
2024-12-30 14:33:24 +0530

    commit

--- skill_tree/server/dist/mongoose/counter.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-// Define the schema for Counter
-const counterSchema = new Schema({
-    id: { type: String, required: true },
-    seq: { type: Number, default: 0 }
-});
-// Create and export the Counter model
-export const Counter = mongoose.model('Counter', counterSchema);
--- skill_tree/server/dist/utils/webtoken.js
+++
@@ -1,2 +0,0 @@
-export const sendVerifcationEmail = (email) => {
-};
